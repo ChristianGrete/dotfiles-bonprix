d9c4fe44e6994faca254e2346927dcf94be0635a
Christian Grete <webmaster@christiangrete.com>
2018-02-16 11:53:44 +0100

    release(v0.0.1-alpha): distribute [ci skip]

---
+++ dist/profile.sh
@@ -0,0 +1,17 @@
+export BONPRIX="$PROJECTS/@bonprix"
+export BONPRIX_PACKAGE="$DOTFILES_PACKAGES/dotfiles-bonprix"
+export BONPRIX_PACKAGE_SOURCES="$BONPRIX_PACKAGE/dist"
+export BONPRIX_PACKAGE_VERSION="$(
+  cd "$BONPRIX_PACKAGE"
+
+  git describe --tags "$(git rev-list --max-count=1 --tags)" 2>/dev/null
+
+  [ $? -ne 0 ] && echo '0.0.0'
+)"
+
+. "$BONPRIX_PACKAGE_SOURCES/lib/cli.sh"
+
+alias bp='bonprix'
+alias bp.='bp .'
+alias bpp='bp pkg'
+alias bppb='bpp build'
