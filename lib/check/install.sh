510ace561315bd3564ef77d98d3c7972ed7a1464
Christian Grete <webmaster@christiangrete.com>
2018-02-19 11:09:17 +0100

    chore(ci): get pwd [WIP]

--- lib/check/install.sh
+++ lib/check/install.sh
@@ -2,3 +2,14 @@ echo '... Installing dotfiles:
 '
 
 "$HOME/.dotfiles/bin/install"
+
+[ $? -eq 0 ] && . "$HOME/.profile"
+
+if [ $? -ne 0 ]
+  then
+    echo 'Error: Unable to install and load dotfiles.' >&2
+
+    exit 1
+fi
+
+echo "$(pwd)"
