a9c07b79713acf66057176ae70b524eafa790e72
Christian Grete <webmaster@christiangrete.com>
2018-02-21 16:47:23 +0100

    test(dist): define `profile.sh` spec

---
+++ lib/test/profile.sh
@@ -0,0 +1,74 @@
+describe '$BONPRIX'
+  it 'is defined'
+    [ -z ${BONPRIX:+BONPRIX} ] && fail || succeed
+  it 'defines an existing directory'
+    [ -d "$BONPRIX" ] && succeed || fail
+  it 'defines a readable directory'
+    [ -r "$BONPRIX" ] && succeed || fail
+
+describe '$BONPRIX_PACKAGE'
+  it 'is defined'
+    [ -z ${BONPRIX_PACKAGE:+BONPRIX_PACKAGE} ] && fail || succeed
+  it 'defines an existing directory'
+    [ -d "$BONPRIX_PACKAGE" ] && succeed || fail
+  it 'defines a readable directory'
+    [ -r "$BONPRIX_PACKAGE" ] && succeed || fail
+  it 'defines a writable directory'
+    [ -w "$BONPRIX_PACKAGE" ] && succeed || fail
+
+describe '$BONPRIX_PACKAGE_SOURCES'
+  it 'is defined'
+    [ -z ${BONPRIX_PACKAGE_SOURCES:+BONPRIX_PACKAGE_SOURCES} ]
+    [ $? -eq 0 ] && fail || succeed
+  it 'defines an existing directory'
+    [ -d "$BONPRIX_PACKAGE_SOURCES" ] && succeed || fail
+  it 'defines a readable directory'
+    [ -r "$BONPRIX_PACKAGE_SOURCES" ] && succeed || fail
+
+describe '$BONPRIX_PACKAGE_VERSION'
+  it 'is defined'
+    [ -z ${BONPRIX_PACKAGE_VERSION:+BONPRIX_PACKAGE_VERSION} ]
+    [ $? -eq 0 ] && fail || succeed
+  it 'is not empty'
+    [ -z "$BONPRIX_PACKAGE_VERSION" ] && fail || succeed
+
+describe '$BONPRIX_PACKAGE_SOURCES/lib/cli.sh'
+  it 'is included'
+    [ "$(bonprix -v 2>&1)" = "$BONPRIX_PACKAGE_VERSION" ]
+    [ $? -eq 0 ] && succeed || fail
+
+describe 'bp'
+  it 'is defined'
+    alias bp >/dev/null 2>&1 && succeed || fail
+
+describe 'bp.'
+  it 'is defined'
+    alias 'bp.' >/dev/null 2>&1 && succeed || fail
+
+describe 'bpp'
+  it 'is defined'
+    alias bpp >/dev/null 2>&1 && succeed || fail
+
+describe 'bpp.'
+  it 'is defined'
+    alias 'bpp.' >/dev/null 2>&1 && succeed || fail
+
+describe 'bppb'
+  it 'is defined'
+    alias bppb >/dev/null 2>&1 && succeed || fail
+
+describe 'bppc'
+  it 'is defined'
+    alias bppc >/dev/null 2>&1 && succeed || fail
+
+describe 'bppi'
+  it 'is defined'
+    alias bppi >/dev/null 2>&1 && succeed || fail
+
+describe 'bppr'
+  it 'is defined'
+    alias bppr >/dev/null 2>&1 && succeed || fail
+
+describe 'bppu'
+  it 'is defined'
+    alias bppu >/dev/null 2>&1 && succeed || fail
