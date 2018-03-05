e00f37d262d4f8c1893733105402ed863a7778bd
Christian Grete <webmaster@christiangrete.com>
2018-03-05 15:28:38 +0100

    chore(install): install brews in setup

--- lib/install/setup.sh
+++ lib/install/setup.sh
@@ -27,3 +27,24 @@ if [ ! -d "$PROJECTS/@bonprix" ]
       exit 1
     fi
 fi
+
+printf 'Installing Homebrew dependencies ... '
+
+$(
+  cd "$PWD/dist"
+
+  brew bundle >/dev/null 2>&1
+)
+
+if [ $? -eq 0 ]
+  then
+    echo 'success.'
+else
+  echo 'failed.'
+
+  echo; echo "Error: Unable to install Homebrew dependencies." >&2
+
+  exit 1
+fi
+
+echo
