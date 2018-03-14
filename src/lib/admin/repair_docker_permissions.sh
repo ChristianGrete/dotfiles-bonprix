533a903a3d7131fe51047cec2de785c4f01791b5
Christian Grete <webmaster@christiangrete.com>
2018-03-14 16:22:08 +0100

    refactor(cli): rename script

--- src/lib/admin/repair_docker_permissions.sh
+++ src/lib/admin/repair_docker_permissions.sh
@@ -1,9 +1,12 @@
-relink_docker () {
-  executables='docker,docker-compose,docker-machine,hyperkit,vpnkit'
+repair_docker_permissions () {
+  executables='docker docker-compose docker-machine hyperkit vpnkit'
   source_path='/Applications/Docker.app/Contents/Resources/bin'
   target_path='/usr/local/bin'
 
-  ln -fs "$source_path/{$executables}" "$target_path"
+  for executable in $executables; do
+    ln -fs "$source_path/$executable" "$target_path"
+  done
+
   ln -fs "$source_path/docker-credential-osxkeychain.bin" \
     "$target_path/docker-credential-osxkeychain"
   ln -fs "$source_path/notary.bin" "$target_path/notary"
